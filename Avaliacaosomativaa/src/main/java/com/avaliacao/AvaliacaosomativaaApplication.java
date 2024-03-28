4068620c08ec66490ec1253b83d16b346c8d8ccf
Lucas Prestes <132359745+Mysteeeeel@users.noreply.github.com>
2024-03-28 16:20:20 -0300

    Add files via upload

---
+++ Avaliacaosomativaa/src/main/java/com/avaliacao/AvaliacaosomativaaApplication.java
@@ -0,0 +1,13 @@
+package com.avaliacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaosomativaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaosomativaaApplication.class, args);
+	}
+
+}
